3f877b8fb8d48713f115f5767b1c1e74c4bd1ac6
josuecristhoper <@josue.cristhoper>
2023-11-02 09:32:23 +0100

    Mi tercer commit

---
+++ Programacion/Practica_Entornos.java
@@ -0,0 +1,21 @@
+
+package practica_entornos;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author josuecristhoper
+ */
+public class Practica_Entornos {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("¿Como te llamas?");
+        String nombre = sc.nextLine();
+        System.out.println("Hola "+nombre);
+        
+    }
+    
+}
